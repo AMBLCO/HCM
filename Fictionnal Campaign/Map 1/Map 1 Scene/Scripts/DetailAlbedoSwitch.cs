816c90da88ae7541bbb4f3b09f0ae967d5b3bf09
AMBLCO <matis.berubelauziere@gmail.com>
2022-01-20 00:40:52 -0500

    Walls + camera positions + opaque windows

---
+++ Fictionnal Campaign/Map 1/Map 1 Scene/Scripts/DetailAlbedoSwitch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+ 
+public class DetailAlbedoSwitch : MonoBehaviour {
+    public Texture DetailAlbedo;
+	public string MaterialName;
+	// Use this for initialization
+	void Update () {
+		Renderer[] meshRenderer = GetComponentsInChildren<Renderer>();
+		if (meshRenderer.Length == 0)
+		{
+			return;
+		}
+	
+        // Set the new material on the GameObject
+		for (int i=0; i<meshRenderer.Length; i++)
+		{
+			var materials = meshRenderer[i].materials;
+			for (int j = 0; j < materials.Length; j++)
+			{
+				if (materials[j].name.Contains(MaterialName))
+				{
+					materials[j].SetTexture("_DetailAlbedoMap", DetailAlbedo);
+				}
+			}
+			meshRenderer[i].materials = materials;
+		}
+		this.enabled = false;
+    }
+}
